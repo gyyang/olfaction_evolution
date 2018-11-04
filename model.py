9de2108dec5c6e090b4d7aa894fde3ed019c41c3
gyyang <gyyang.neuro@gmail.com>
2018-11-04 16:38:07 -0500

    consolidating peter model

--- model.py
+++ model.py
@@ -6,10 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import tensorflow as tf
-import pickle as pkl
 
-import input
+import task
 
+
+N_GLO = 50
+N_KC = 2500
+N_CLASS = 1000
 
 class modelConfig():
     lr = .001
@@ -26,8 +29,8 @@ def make_input(x, y, batch_size):
     return train_iter, next_element
 
 
-class Model(object):
-    """Network model."""
+class SingleLayerModel(object):
+    """Model."""
 
     def __init__(self, x, y):
         """Make model.
@@ -36,30 +39,88 @@ class Model(object):
             x: tf placeholder or iterator element (batch_size, N_ORN)
             y: tf placeholder or iterator element (batch_size, N_GLO)
         """
-        config = modelConfig()
-        lr = config.lr
+        self.config = modelConfig()
 
-        input_config = input.smallConfig()
+        input_config = task.smallConfig()
         y_dim = input_config.N_ORN
 
         self.logits = tf.layers.dense(x, y_dim)
         self.predictions = tf.sigmoid(self.logits)
         xe_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels = y, logits = self.logits)
         self.loss = tf.reduce_mean(xe_loss)
-        optimizer = tf.train.AdamOptimizer(lr)
+
+        optimizer = tf.train.AdamOptimizer(self.config.lr)
         self.train_op = optimizer.minimize(self.loss)
 
-    def train(self, sess):
+
+class FullModel(object):
+    """Model."""
+
+    def __init__(self, x, y):
+        """Make model.
+
+        Args:
+            x: tf placeholder or iterator element (batch_size, N_ORN)
+            y: tf placeholder or iterator element (batch_size, N_GLO)
+        """
+        self.config = modelConfig()
+
+        glo = tf.layers.dense(x, N_GLO, activation=tf.nn.relu)
+        kc = tf.layers.dense(glo, N_KC, activation=tf.nn.relu)
+        logits = tf.layers.dense(kc, N_CLASS)
+
+        self.loss = tf.losses.sparse_softmax_cross_entropy(labels=y,
+                                                           logits=logits)
+        pred = tf.argmax(logits, axis=-1)
+        # pred = tf.cast(pred, tf.int32)
+        # acc = tf.reduce_mean(tf.equal(y_target, pred))
+        self.acc = tf.metrics.accuracy(labels=y, predictions=pred)
+
+        optimizer = tf.train.AdamOptimizer(self.config.lr)
+        self.train_op = optimizer.minimize(self.loss)
+
+
+if __name__ == '__main__':
+    experiment = 'peter'
+    if experiment == 'peter':
+        features, labels = task.generate_repeat()
+        Model = SingleLayerModel
+    elif experiment == 'robert':
+        features, labels = task.generate_proto()
+        Model = FullModel
+    else:
+        raise NotImplementedError
+
+    config = modelConfig()
+    save_path = config.save_path
+    batch_size = config.batch_size
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    n_batch = features.shape[0] // batch_size
+
+    features_placeholder = tf.placeholder(features.dtype, features.shape)
+    labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
+
+    train_iter, next_element = make_input(features_placeholder, labels_placeholder, batch_size)
+
+    model = Model(next_element[0], next_element[1])
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(train_iter.initializer,
+                 feed_dict={features_placeholder: features,
+                            labels_placeholder: labels})
+
         loss = 0
-        for ep in range(n_epoch):
+        for ep in range(config.epoch):
             for b in range(n_batch):
                 # cur_inputs, cur_labels = sess.run(next_element)
                 # feed_dict = {self.x: cur_inputs, self.y: cur_labels}
                 # output, loss, _ = sess.run([self.predictions, self.loss, self.train_op], feed_dict)
-                output, loss, _ = sess.run(
-                    [self.predictions, self.loss, self.train_op])
+                loss, _ = sess.run([model.loss, model.train_op])
 
-            if (ep %2 == 0 and ep!= 0):
+            if (ep % 2 == 0 and ep != 0):
                 print('[*] Epoch %d  total_loss=%.2f' % (ep, loss))
 
             # if (ep%10 ==0 and ep != 0):
@@ -79,43 +140,5 @@ class Model(object):
             #     fig.savefig(path_name, bbox_inches='tight',dpi=300)
             #     plt.close(fig)
 
-    def test(self, sess, x, y):
-        raise NotImplementedError
-        feed_dict = {self.x: x, self.y: y}
-        output, loss = sess.run([self.predictions, self.loss], feed_dict)
-        print('[!] Test_loss=%.2f' % (loss))
-        return loss
-
-
-if __name__ == '__main__':
-    in_x, in_y = input.generate()
-    x_test, y_test = input.generate()
-
-    input_config = input.smallConfig()
-
-    config = modelConfig()
-    save_path = config.save_path
-    n_epoch = config.epoch
-    batch_size = config.batch_size
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    n_batch = in_x.shape[0] // batch_size
-    features = in_x
-    labels = in_y
-    features_placeholder = tf.placeholder(features.dtype, features.shape)
-    labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
-
-    train_iter, next_element = make_input(features_placeholder, labels_placeholder, batch_size)
-    model = Model(next_element[0], next_element[1])
-
-
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        sess.run(train_iter.initializer,
-                 feed_dict={features_placeholder: features,
-                            labels_placeholder: labels})
-
-        model.train(sess)
 
 
--- task.py
+++ task.py
@@ -1,24 +1,48 @@
 import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
 
-N_CLASS = 1000
-N_ORN = 50
 
-prototypes = np.random.rand(N_CLASS, N_ORN)
+def generate_proto():
+    N_CLASS = 1000
+    N_ORN = 50
+
+    prototypes = np.random.rand(N_CLASS, N_ORN)
+
+    N_TRAIN = 100000
+    N_VAL = 1000
+    train_odors = np.random.rand(N_TRAIN, N_ORN)
+    val_odors = np.random.rand(N_VAL, N_ORN)
+
+    def get_labels(odors):
+        dist = euclidean_distances(prototypes, odors)
+        return np.argmax(dist, axis=0)
+
+    train_labels = get_labels(train_odors)
+    val_labels = get_labels(val_odors)
+    return train_odors, train_labels
 
 
-# TODO: Activate a proportion of ORN
-# TODO: Sparse PN to KC, each KC receives 7/50
-# TODO: (optional) fix the weight from PN to KC
+class smallConfig():
+    N_SAMPLES = 1000
 
-N_TRAIN = 100000
-N_VAL = 1000
-train_odors = np.random.rand(N_TRAIN, N_ORN)
-val_odors = np.random.rand(N_VAL, N_ORN)
+    N_ORN = 30
+    NEURONS_PER_ORN = 50
+    NOISE_STD = .2
 
-def get_labels(odors):
-    dist = euclidean_distances(prototypes, odors)
-    return np.argmax(dist, axis=0)
+def generate_repeat():
+    '''
+    :return:
+    x = noisy ORN channels. n_samples X n_orn * neurons_per_orn
+    y = noiseless PN channels
+    '''
+    config = smallConfig()
+    n_samples = config.N_SAMPLES
+    neurons_per_orn = config.NEURONS_PER_ORN
+    n_orn = config.N_ORN
+    noise_std = config.NOISE_STD
 
-train_labels = get_labels(train_odors)
-val_labels = get_labels(val_odors)
+    y = np.random.uniform(low=0, high=1, size= (n_samples, n_orn))
+    x = np.repeat(y, repeats = neurons_per_orn, axis = 1)
+    n = np.random.normal(loc=0, scale= noise_std, size= x.shape)
+    x += n
+    return x.astype(np.float32), y.astype(np.float32)
