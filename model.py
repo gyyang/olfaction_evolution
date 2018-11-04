812f3d7191e29e59c5e2bf037194ae87c97d29a5
gyyang <gyyang.neuro@gmail.com>
2018-11-04 12:07:47 -0500

    formatting

--- model.py
+++ model.py
@@ -1,17 +1,22 @@
+"""Model file."""
+
+import os
+
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-import input
+import tensorflow as tf
 import pickle as pkl
-import seaborn as sns
-import os
+
+import input
+
 
 class modelConfig():
     lr = .001
     epoch = 100
     batch_size = 100
     save_path = './files'
+
 
 def make_input(x, y, batch_size):
     data = tf.data.Dataset.from_tensor_slices((x, y))
@@ -20,44 +25,31 @@ def make_input(x, y, batch_size):
     next_element = train_iter.get_next()
     return train_iter, next_element
 
-class model():
-    '''
-    Goal of task is to ask whether the same type of ORN neurons can be made to connect to PNs to de-noise ORN inputs
-    '''
+
+class Model(object):
+    """Network model."""
+
     def __init__(self, x, y):
+        """Make model.
+
+        Args:
+            x: tf placeholder or iterator element (batch_size, N_ORN)
+            y: tf placeholder or iterator element (batch_size, N_GLO)
+        """
         config = modelConfig()
         lr = config.lr
 
         input_config = input.smallConfig()
         y_dim = input_config.N_ORN
 
-        self.x = x
-        self.y = y
-        # self.w = tf.get_variable('W', shape=[x_dim,y_dim], dtype=tf.float32)
-        # b = tf.get_variable('B', shape=[y_dim], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
-
-        self.logits = tf.layers.dense(self.x, y_dim)
-        # self.logits = tf.matmul(self.x, self.w) + b
+        self.logits = tf.layers.dense(x, y_dim)
         self.predictions = tf.sigmoid(self.logits)
-        xe_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels = self.y, logits = self.logits)
+        xe_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels = y, logits = self.logits)
         self.loss = tf.reduce_mean(xe_loss)
         optimizer = tf.train.AdamOptimizer(lr)
         self.train_op = optimizer.minimize(self.loss)
 
     def train(self, sess):
-        # input_config = input.smallConfig()
-        #
-        # config = modelConfig()
-        # save_path = config.save_path
-        # n_epoch = config.epoch
-        # batch_size = config.batch_size
-        # if not os.path.exists(save_path):
-        #     os.makedirs(save_path)
-        #
-        # n_batch = x.shape[0]//batch_size
-        # train_iter, next_element= make_input(x, y, batch_size)
-        # sess.run(train_iter.initializer, {self.x: x, self.y: y})
-
         loss = 0
         for ep in range(n_epoch):
             for b in range(n_batch):
@@ -88,16 +80,11 @@ class model():
             #     plt.close(fig)
 
     def test(self, sess, x, y):
+        raise NotImplementedError
         feed_dict = {self.x: x, self.y: y}
         output, loss = sess.run([self.predictions, self.loss], feed_dict)
         print('[!] Test_loss=%.2f' % (loss))
         return loss
-
-def see():
-    #TODO
-    with open('.W.pkl','rb') as f:
-        mat = pkl.load(f)
-
 
 
 if __name__ == '__main__':
@@ -120,7 +107,7 @@ if __name__ == '__main__':
     labels_placeholder = tf.placeholder(labels.dtype, labels.shape)
 
     train_iter, next_element = make_input(features_placeholder, labels_placeholder, batch_size)
-    network = model(next_element[0], next_element[1])
+    model = Model(next_element[0], next_element[1])
 
 
     with tf.Session() as sess:
@@ -129,6 +116,6 @@ if __name__ == '__main__':
                  feed_dict={features_placeholder: features,
                             labels_placeholder: labels})
 
-        network.train(sess)
+        model.train(sess)
 
 
