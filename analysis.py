cf7b8706e1c6377fb4cba8f4eeb5493910b95b9f
gyyang <gyyang.neuro@gmail.com>
2018-11-05 14:12:09 -0500

    moving glo score to tools so it can be used elsewhere

--- analysis.py
+++ analysis.py
@@ -6,45 +6,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-def compute_glo_score(w_orn):
-    """Compute the glomeruli score.
-
-    This function returns the glomeruli score, a number between 0 and 1 that
-    measures how close the connectivity is to glomeruli connectivity.
-
-    For one glomeruli neuron, first we compute the average connections from
-    each ORN group. Then we sort the absolute connection weights by ORNs.
-    The glomeruli score is simply:
-        (Max weight - Second max weight) / (Max weight + Second max weight)
-
-    Args:
-        w_orn: numpy array (n_neuron, n_orn). This matrix has to be organized
-        in the following way: the n_neuron neurons are grouped into n_orn groups
-        index 0, ..., n_neuron_per_orn - 1 is the 0-th group, and so on
-
-    Return:
-        avg_glo_score: scalar, average glomeruli score
-        glo_scores: numpy array (n_orn,), all glomeruli scores
-    """
-    n_neuron, n_orn = w_orn.shape
-    n_neuron_per_orn = n_neuron // n_orn
-    w_orn_by_orn = np.reshape(w_orn, (n_orn, n_neuron_per_orn, n_orn))
-    w_orn_by_orn = w_orn_by_orn.mean(axis=1)
-    w_orn_by_orn = abs(w_orn_by_orn)
-
-    glo_scores = list()
-    for i in range(n_orn):
-        w_tmp = w_orn_by_orn[:, i]  # all projections to the i-th PN neuron
-        indsort = np.argsort(w_tmp)[::-1]
-        w_max = w_tmp[indsort[0]]
-        w_second = w_tmp[indsort[1]]
-        glo_score = (w_max - w_second) / (w_max + w_second)
-        glo_scores.append(glo_score)
-
-    avg_glo_score = np.mean(glo_scores)
-    return avg_glo_score, glo_scores
+import tools
 
 
 def load_w_orn(model_dir):
@@ -63,7 +25,7 @@ epochs = np.sort([int(m) for m in model_dirs])
 model_dirs = [str(m) for m in epochs]
 
 w_orns = [load_w_orn(m) for m in model_dirs]
-glo_score_list = [compute_glo_score(w)[0] for w in w_orns]
+glo_score_list = [tools.compute_glo_score(w)[0] for w in w_orns]
 
 plt.figure()
 plt.plot(epochs, glo_score_list)
