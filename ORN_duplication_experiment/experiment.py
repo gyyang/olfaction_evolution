c07178901d33cd5eede24e41cb954652b1b08e6c
Peter <peterwang724@gmail.com>
2018-11-07 10:49:19 -0500

    added more normalization parameters

--- ORN_duplication_experiment/experiment.py
+++ ORN_duplication_experiment/experiment.py
@@ -4,18 +4,31 @@ import os
 import task
 import train
 import configs
+from collections import OrderedDict
+import numpy as np
 
 def varying_config(i):
-    # Ranges of hyperparameters to loop over
-    noise = [0, .25, .5]
+    config = configs.FullConfig()
+    config.save_path = './files/' + str(i).zfill(2)
+    config.N_ORN_PER_PN = 10
 
-    if i >= len(noise):
+    # Ranges of hyperparameters to loop over
+    hp_ranges = OrderedDict()
+    hp_ranges['ORN_NOISE_STD'] = [0, .25, .5]
+
+    # Unravel the input index
+    keys = hp_ranges.keys()
+    dims = [len(hp_ranges[k]) for k in keys]
+    n_max = np.prod(dims)
+
+    if i >= n_max:
         return
 
-    config = configs.FullConfig()
-    config.save_path = './files_' + str(i).zfill(2)
-    config.N_ORN_PER_PN = 10
-    config.ORN_NOISE_STD = noise[i]
+    indices = np.unravel_index(i % n_max, dims=dims)
+    # Set up new hyperparameter
+    for key, index in zip(keys, indices):
+        setattr(config, key, hp_ranges[key][index])
+    train.train(config)
 
     data_path = task.save_proto(config)
     config.data_dir = data_path
@@ -24,5 +37,5 @@ def varying_config(i):
 
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    for i in range(1,100):
+    for i in range(0,100):
         varying_config(i)
