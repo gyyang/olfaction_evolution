8416a957d98c236707cd7b6736ffb71fe96c7171
gyyang <gyyang.neuro@gmail.com>
2018-11-09 17:48:52 -0500

    made more portable

--- schematics/cosyne_figs.py
+++ schematics/cosyne_figs.py
@@ -1,32 +1,43 @@
+import sys
 import os
 import pickle
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+
+rootpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(rootpath)  # This is hacky, should be fixed
+
 import tools
 
 mpl.rcParams['font.size'] = 7
 
-dir = 'C:/Users/Peter/PycharmProjects/olfaction_evolution/vary_size_experiment/vary_PN/files'
+new_rootpath = os.path.join(rootpath, 'files', 'vary_size_experiment')
+
+dir = os.path.join(new_rootpath, 'vary_PN', 'files')
 dirs = [os.path.join(dir, n) for n in os.listdir(dir)]
 fig_dir = os.path.join(os.getcwd())
 
-glo_score, val_acc = [], []
+glo_score = np.zeros(len(dirs))
+val_acc = np.zeros(len(dirs))
+n_pns = np.zeros(len(dirs))
+n_kcs = np.zeros(len(dirs))
 for i, d in enumerate(dirs):
     log_name = os.path.join(d, 'log.pkl')
     with open(log_name, 'rb') as f:
         log = pickle.load(f)
-    glo_score.append(log['glo_score'])
-    val_acc.append(log['val_acc'])
+    config = tools.load_config(d)
+    glo_score[i] = log['glo_score'][-1]
+    val_acc[i] = log['val_acc'][-1]
+    n_pns[i] = config.N_GLO
+    n_kcs[i] = config.N_KC
 
-parameters =np.array(list(range(10,110,10)) + [150, 200, 250])
-
+ind_sort = np.argsort(n_pns)
 mpl.rcParams['font.size'] = 10
 fig = plt.figure(figsize=(2, 2))
 ax = fig.add_axes([0.3, 0.3, 0.6, 0.6])
-y = [d[-1] for d in glo_score]
-ax.plot(np.log10(parameters), y)
+ax.plot(np.log10(n_pns[ind_sort]), glo_score[ind_sort], 'o-')
 ax.set_xlabel('nPNs')
 ax.set_ylabel('Glo Score')
 ax.spines["right"].set_visible(False)
@@ -42,25 +53,30 @@ plt.xticks(np.log10(ix), ix)
 
 plt.savefig(os.path.join(fig_dir, 'vary_PN.pdf'))
 
-dir = 'C:/Users/Peter/PycharmProjects/olfaction_evolution/vary_size_experiment/vary_KC/files'
+dir = os.path.join(new_rootpath, 'vary_KC', 'files')
 dirs = [os.path.join(dir, n) for n in os.listdir(dir)]
 fig_dir = os.path.join(os.getcwd())
 
-glo_score, val_acc = [], []
+glo_score = np.zeros(len(dirs))
+val_acc = np.zeros(len(dirs))
+n_pns = np.zeros(len(dirs))
+n_kcs = np.zeros(len(dirs))
 for i, d in enumerate(dirs):
     log_name = os.path.join(d, 'log.pkl')
     with open(log_name, 'rb') as f:
         log = pickle.load(f)
-    glo_score.append(log['glo_score'])
-    val_acc.append(log['val_acc'])
-
-parameters =np.array([50, 100, 200, 400, 800, 1200, 2500, 5000, 10000, 20000])
+    config = tools.load_config(d)
+    glo_score[i] = log['glo_score'][-1]
+    val_acc[i] = log['val_acc'][-1]
+    n_pns[i] = config.N_GLO
+    n_kcs[i] = config.N_KC
+    
+ind_sort = np.argsort(n_kcs)
 
 mpl.rcParams['font.size'] = 10
 fig = plt.figure(figsize=(2, 2))
 ax = fig.add_axes([0.3, 0.3, 0.6, 0.6])
-y = [d[-1] for d in glo_score]
-ax.plot(np.log10(parameters), y)
+ax.plot(np.log10(n_kcs[ind_sort]), glo_score[ind_sort])
 ax.set_xlabel('nKCs')
 ax.set_ylabel('Glo Score')
 ax.spines["right"].set_visible(False)
