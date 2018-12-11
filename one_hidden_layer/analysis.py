da7126441c24b5430a78edce10330f761e314d9a
Shuying Luo <shuying.luo@gmail.com>
2018-12-10 23:21:05 -0500

    Update experiment and analysis

--- one_hidden_layer/analysis.py
+++ one_hidden_layer/analysis.py
@@ -4,32 +4,38 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import task
+import tools
 import train
 from model import NormalizedMLP
 import tensorflow as tf
 import numpy as np
 import one_hidden_layer.experiment as train_config
-import standard.analysis
+from os.path import expanduser
 
-root_path = os.path.dirname(os.path.abspath(__file__))
+home = expanduser("~")
+root_path = os.path.dirname(os.getenv("HOME"))
 sys.path.append(root_path)  # This is hacky, should be fixed
-
 
 mpl.rcParams['font.size'] = 7
 
 save_name = 'one_hidden_layer'
-save_path = os.path.join(root_path, 'files', save_name)
+save_path = os.path.join(home, 'Dropbox', save_name)
 
 fig_dir = os.path.join(root_path, 'figures')
 
 # Rebuild model to
 CurrentModel = NormalizedMLP
-config = train_config.config
-# hacky way to run it on CPU
-config.save_path = '.'+ config.save_path
+config = train_config.get_config()
+data_dir = os.path.join(save_path, 'dataset')
+config.save_path = os.path.join(save_path, 'files')
+config.data_dir = data_dir
+
+dataset_config = tools.load_config(config.data_dir)
+dataset_config.update(config)
+config = dataset_config
+
 train_x, train_y, val_x, val_y = task.load_data(config.dataset,
-                                                # hacky way to run it on CPU
-                                                '.'+config.data_dir)
+                                                data_dir)
 # Build train model
 train_x_ph = tf.placeholder(train_x.dtype, train_x.shape)
 train_y_ph = tf.placeholder(train_y.dtype, train_y.shape)
@@ -44,11 +50,10 @@ with tf.Session(config=tf_config) as sess:
     sess.run(train_iter.initializer, feed_dict={train_x_ph: train_x,
                                                 train_y_ph: train_y})
     model.load()
-    results = sess.run([v for v in tf.trainable_variables() if v.name=='model/layer1/kernel:0'])
+    results = sess.run([v for v in tf.trainable_variables() if v.name == 'model/layer1/kernel:0'])
     orn_to_kc_w = results[0]
     print(orn_to_kc_w.shape)
     U, S, V = np.linalg.svd(orn_to_kc_w)
-    plt.plot(S)
+    print(S)
+    plt.hist(S)
     plt.show()
-
-standard.analysis.plot_weights()
--- one_hidden_layer/experiment.py
+++ one_hidden_layer/experiment.py
@@ -7,19 +7,22 @@ import task
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
-def train_one_layer():
-    task_config = task.input_ProtoConfig()
-    task.save_proto(config=task_config, seed=0, folder_name='one_hidden_layer')
-
+def get_config():
     config = configs.FullConfig()
     config.max_epoch = 30
-    config.ORN_NOISE_STD = 0.0
+    config.ORN_NOISE_STD = 0.1
     # simulate connecting directly to KC
     config.NEURONS = [2500]
     config.model = 'normmlp'
     config.data_dir = './datasets/proto/one_hidden_layer'
     config.save_path = './files/one_hidden_layer'
-    train.train(config)
+    return config
+
+
+def train_one_layer():
+    task_config = task.input_ProtoConfig()
+    task.save_proto(config=task_config, seed=0, folder_name='one_hidden_layer')
+    train.train(get_config())
 
 
 if __name__ == '__main__':
