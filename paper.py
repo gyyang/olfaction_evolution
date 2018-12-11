d958b7ef57f9cbaf4af4c355be263c2a31dc0918
gyyang <gyyang.neuro@gmail.com>
2018-12-10 23:35:52 -0500

    including vary duplication

--- paper.py
+++ paper.py
@@ -1,17 +1,21 @@
 """File that summarizes all key results."""
 
+import standard.experiment as standard_experiment
+import standard.analysis as standard_analysis
+import vary_noise.experiment as vary_noise_experiment
+from vary_noise.experiment import local_train
+
 # MODE = 'train'
 MODE = 'analysis'
 
 # Reproducing glomeruli-like activity
-import standard.experiment as standard_experiment
-import standard.analysis as standard_analysis
-save_path = './files/standard/orn2pn'
-if MODE == 'train':
-    standard_experiment.train_orn2pn(save_path)
-else:
-    standard_analysis.plot_progress(save_path)
-    standard_analysis.plot_weights(save_path)
+# save_path = './files/standard/orn2pn'
+# if MODE == 'train':
+#     standard_experiment.train_orn2pn(save_path)
+# else:
+#     standard_analysis.plot_progress(save_path)
+#     standard_analysis.plot_weights(save_path)
+#     # TODO: Add activity distribution
 
 # Varying #PN and #KC
 if MODE == 'train':
@@ -20,10 +24,24 @@ else:
     pass
 
 # Varying the noise level while varying #KC
+# save_path = './files/vary_noise3'
+# if MODE == 'train':
+#     local_train(vary_noise_experiment.vary_kc_configs, save_path)
+# else:
+#     standard_analysis.plot_results(save_path, x_key='N_KC', y_key='glo_score',
+#                                    loop_key='ORN_NOISE_STD')
+#     standard_analysis.plot_results(save_path, x_key='N_KC', y_key='val_acc',
+#                                    loop_key='ORN_NOISE_STD')
+
+# Varying ORN duplication numbers
+save_path = './files/vary_n_orn_duplication'
 if MODE == 'train':
-    pass
+    local_train(vary_noise_experiment.vary_n_orn_duplication, save_path)
 else:
-    pass
+    standard_analysis.plot_results(
+        save_path, x_key='N_ORN_DUPLICATION', y_key='glo_score')
+    standard_analysis.plot_results(
+        save_path, x_key='N_ORN_DUPLICATION', y_key='val_acc')
 
 # Reproducing sparse PN-KC connectivity
 if MODE == 'train':
