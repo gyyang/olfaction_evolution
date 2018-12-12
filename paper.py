4d7dca918cfe1010c029e278dff30bae12f483b9
gyyang <gyyang.neuro@gmail.com>
2018-12-12 16:06:47 -0500

    major restructuring of paper.py

--- paper.py
+++ paper.py
@@ -15,152 +15,83 @@ ANALYZE = True
 TESTING_EXPERIMENTS = True
 run_ix = [6]
 
+# experiments
+experiments = ['orn2pn', 'vary_ORN_duplication', 'vary_PN', 'vary_KC',
+               'vary_KC_claws', 'train_KC_claws', 'random_KC_claws']
+# experiments = ['train_KC_claws', 'random_KC_claws']
 
-save_paths = ['./files/orn2pn',
-              './files/vary_ORN_duplication',
-              './files/vary_PN',
-              './files/vary_KC',
-              './files/vary_KC_claws',
-              './files/train_KC_claws',
-              './files/random_KC_claws'
-              ]
+is_test = True
 
-experiments = [standard_experiment.train_orn2pn, # Reproducing glomeruli-like activity
-               standard_experiment.vary_orn_duplication_configs, # Vary ORN n duplication under different nKC
-               standard_experiment.vary_pn_configs, # Vary nPN under different noise levels
-               standard_experiment.vary_kc_configs, # Vary nKC under different noise levels
-               standard_experiment.vary_claw_configs, # Vary nClaws under different noise levels
-               standard_experiment.train_claw_configs, # Compare training vs fixing weights for PN2KC
-               standard_experiment.random_claw_configs, # Analyze randomness of PN2KC connectivity
-               ]
-
-run_methods = [local_train,
-               local_train,
-               local_train,
-               local_train,
-               local_train,
-               local_sequential_train,
-               local_train
-               ]
-
-analysis_methods_per_experiment = [
-    [standard_analysis.plot_progress,
-     standard_analysis.plot_weights],
-    [lambda x: standard_analysis.plot_results(x, x_key='N_ORN_DUPLICATION', y_key='glo_score',
-                                              loop_key='N_KC'),
-     lambda x: standard_analysis.plot_results(x, x_key='N_ORN_DUPLICATION', y_key='val_acc',
-                                              loop_key='N_KC')],
-    [lambda x: standard_analysis.plot_results(x, x_key='N_PN', y_key='glo_score',
-                                              loop_key='ORN_NOISE_STD'),
-     lambda x: standard_analysis.plot_results(x, x_key='N_PN', y_key='val_acc',
-                                              loop_key='ORN_NOISE_STD')],
-    [lambda x: standard_analysis.plot_results(x, x_key='N_KC', y_key='glo_score',
-                                              loop_key='ORN_NOISE_STD'),
-     lambda x: standard_analysis.plot_results(x, x_key='N_KC', y_key='val_acc',
-                                              loop_key='ORN_NOISE_STD')],
-    [lambda x: standard_analysis.plot_results(x, x_key='kc_inputs', y_key='val_acc',
-                                              loop_key='ORN_NOISE_STD')],
-    [lambda x: standard_analysis.plot_progress(x, alpha= .75, linestyles= [':', '-.', '-'],
-                                               legends= ['Trainable, no loss', 'Trainable, with loss', 'Fixed']),
-     pn2kc_training_analysis.plot_distribution,
-     pn2kc_training_analysis.plot_sparsity],
-    [pn2kc_random_analysis.plot_distribution,
-     lambda x: pn2kc_random_analysis.claw_distribution(x, 'random'),
-     lambda x: pn2kc_random_analysis.plot_cosine_similarity(x, 'preserve', log=False),
-     lambda x: pn2kc_random_analysis.plot_cosine_similarity(x, 'random', log=False),
-     lambda x: pn2kc_random_analysis.pair_distribution(x, 'preserve'),
-     lambda x: pn2kc_random_analysis.pair_distribution(x, 'random')
-     ]
-]
-
-def wrapper(experiment):
-    return lambda: experiment(TESTING_EXPERIMENTS)
-
-for i in run_ix:
-    save_path = save_paths[i]
-    experiment = wrapper(experiments[i])
-    run_method = run_methods[i]
-    analysis_methods = analysis_methods_per_experiment[i]
+if 'orn2pn' in experiments:
+    # Reproducing glomeruli-like activity
+    path = './files/orn2pn'
     if TRAIN:
-        run_method(experiment, save_path)
+        local_train(standard_experiment.train_orn2pn(is_test), path)
     if ANALYZE:
-        for analysis_method in analysis_methods:
-            analysis_method(save_path)
+        standard_analysis.plot_progress(path)
+        standard_analysis.plot_weights(path)
+
+if 'vary_ORN_duplication' in experiments:
+    # Vary ORN n duplication under different nKC
+    path = './files/vary_ORN_duplication'
+    if TRAIN:
+        local_train(standard_experiment.vary_orn_duplication_configs(is_test), path)
+    if ANALYZE:
+        standard_analysis.plot_results(path, x_key='N_ORN_DUPLICATION',
+                                       y_key='glo_score', loop_key='N_KC'),
+        standard_analysis.plot_results(path, x_key='N_ORN_DUPLICATION',
+                                       y_key='val_acc', loop_key='N_KC')
+
+if 'vary_PN' in experiments:
+    # Vary nPN under different noise levels
+    path = './files/vary_PN'
+    if TRAIN:
+        local_train(standard_experiment.vary_pn_configs(is_test), path)
+    if ANALYZE:
+        standard_analysis.plot_results(path, x_key='N_PN', y_key='glo_score',
+                                       loop_key='ORN_NOISE_STD'),
+        standard_analysis.plot_results(path, x_key='N_PN', y_key='val_acc',
+                                       loop_key='ORN_NOISE_STD')
+
+if 'vary_KC' in experiments:
+    # Vary nKC under different noise levels
+    path = './files/vary_KC'
+    if TRAIN:
+        local_train(standard_experiment.vary_kc_configs(is_test), path)
+    if ANALYZE:
+        standard_analysis.plot_results(path, x_key='N_KC', y_key='glo_score',
+                                       loop_key='ORN_NOISE_STD'),
+        standard_analysis.plot_results(path, x_key='N_KC', y_key='val_acc',
+                                       loop_key='ORN_NOISE_STD')
+
+if 'var_KC_claws' in experiments:
+    path = './files/vary_KC_claws'
+    if TRAIN:
+        local_train(standard_experiment.vary_claw_configs(is_test), path)
+    if ANALYZE:
+        standard_analysis.plot_results(path, x_key='kc_inputs', y_key='val_acc',
+                                       loop_key='ORN_NOISE_STD')
+
+if 'train_KC_claws' in experiments:
+    path = './files/train_KC_claws'
+    if TRAIN:
+        local_sequential_train(standard_experiment.train_claw_configs(is_test), path)
+    if ANALYZE:
+        standard_analysis.plot_progress(
+            path, alpha=.75, linestyles=[':', '-.', '-'],
+            legends=['Trainable, no loss', 'Trainable, with loss', 'Fixed']),
+        pn2kc_training_analysis.plot_distribution(path)
+        pn2kc_training_analysis.plot_sparsity(path)
 
 
-
-
-# # Reproducing glomeruli-like activity
-# save_path = './files/standard/orn2pn'
-# if MODE == 'train':
-#     standard_experiment.train_orn2pn(save_path)
-# else:
-#     standard_analysis.plot_progress(save_path)
-#     standard_analysis.plot_weights(save_path)
-#     # TODO: Add activity distribution
-#
-# # Vary nPN under different noise levels
-# save_path = './files/vary_PN'
-# if MODE == 'train':
-#     local_train(wrapper(standard_experiment.vary_pn_configs), save_path)
-# else:
-#     standard_analysis.plot_results(save_path, x_key='N_PN', y_key='glo_score',
-#                                    loop_key='ORN_NOISE_STD')
-#     standard_analysis.plot_results(save_path, x_key='N_PN', y_key='val_acc',
-#                                    loop_key='ORN_NOISE_STD')
-#
-# # Vary nKC under different noise levels
-# save_path = './files/vary_KC'
-# if MODE == 'train':
-#     local_train(standard_experiment.vary_kc_configs, save_path)
-# else:
-#     standard_analysis.plot_results(save_path, x_key='N_KC', y_key='glo_score',
-#                                    loop_key='ORN_NOISE_STD')
-#     standard_analysis.plot_results(save_path, x_key='N_KC', y_key='val_acc',
-#                                    loop_key='ORN_NOISE_STD')
-#
-# # Vary ORN n duplication under different nKC
-# save_path = './files/vary_n_orn_duplication'
-# if MODE == 'train':
-#     local_train(standard_experiment.vary_orn_duplication_configs, save_path)
-# else:
-#     standard_analysis.plot_results(
-#         save_path, x_key='N_ORN_DUPLICATION', y_key='glo_score')
-#     standard_analysis.plot_results(
-#         save_path, x_key='N_ORN_DUPLICATION', y_key='val_acc')
-
-# Reproducing sparse PN-KC connectivity
-if TRAIN == 'train':
-    pass
-else:
-    pass
-
-# Varying PN-KC connectivity sparseness
-if TRAIN == 'train':
-    pass
-else:
-    pass
-
-# Reproducing random connectivity
-if TRAIN == 'train':
-    pass
-else:
-    pass
-
-# The impact of various normalization
-if TRAIN == 'train':
-    pass
-else:
-    pass
-
-# The impact of various task variants
-if TRAIN == 'train':
-    pass
-else:
-    pass
-
-# A search of various hyperparameters
-if TRAIN == 'train':
-    pass
-else:
-    pass
+if 'random_KC_claws' in experiments:
+    path = './files/random_KC_claws'
+    if TRAIN:
+        local_train(standard_experiment.random_claw_configs(is_test), path)
+    if ANALYZE:
+        pn2kc_random_analysis.plot_distribution(path)
+        pn2kc_random_analysis.claw_distribution(path, 'random')
+        pn2kc_random_analysis.plot_cosine_similarity(path, 'preserve', log=False)
+        pn2kc_random_analysis.plot_cosine_similarity(path, 'random', log=False)
+        pn2kc_random_analysis.pair_distribution(path, 'preserve')
+        pn2kc_random_analysis.pair_distribution(path, 'random')
