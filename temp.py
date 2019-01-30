235b31efdc3e79ce1d0dfb696c4a9a318faaa5af
Peter <peterwang724@gmail.com>
2019-01-30 18:25:37 -0500

    1. added diagonal weight initialization to rnn model 2. added option for extra layer in standard model 3. current unfinished code for running rnn model is in RNN_temp.py

--- temp.py
+++ temp.py
@@ -1,5 +1,3 @@
-"""A collection of experiments."""
-
 import os
 import task
 import configs
@@ -9,7 +7,9 @@ import tools
 import train
 import standard.analysis as sa
 import pickle
+import model as network_models
 import standard.analysis_activity as analysis_activity
+import standard.analysis_pn2kc_training as analysis_training
 
 import matplotlib.pyplot as plt
 
@@ -89,7 +89,6 @@ def why_kc_layer():
 # t(temp_oracle(), path, s=0, e=100)
 # sa.plot_results(path, x_key='n_trueclass', y_key='val_acc', loop_key='model')
 
-
 def temp():
     config = configs.FullConfig()
     config.data_dir = './datasets/proto/standard'
@@ -106,114 +105,28 @@ def temp():
     hp_ranges['data_dir'] = ['./datasets/proto/' + str(i) + '_20' for i in x]
     return config, hp_ranges
 
-# path = './files_temp/temp_nkc'
-# t(temp(), path, s=0, e=100)
-# sa.plot_results(path, x_key='n_trueclass', y_key='val_acc', loop_key='N_KC')
-
-def temp_rnn():
+def temp1():
     config = configs.FullConfig()
-    config.data_dir = './datasets/proto/standard'
-    config.max_epoch = 12
-    config.model = 'full'
+    config.data_dir = './datasets/proto/200_20'
+    config.max_epoch = 20
 
     config.replicate_orn_with_tiling = False
     config.N_ORN_DUPLICATION = 1
-    config.ORN_NOISE_STD = 0
     config.skip_orn2pn = True
-    config.train_pn2kc = True
-    config.sparse_pn2kc = False
-    config.train_kc_bias = False
-    config.kc_norm_post = 'batch_norm'
-    # config.NEURONS = 2500
-    # config.TIME_STEPS =1
-    # config.WEIGHT_LOSS = False
-    # config.WEIGHT_ALPHA = 3
-    #
-    # config.dropout = True
-    # config.dropout_rate = .5
+
+    # config.train_kc_bias = False
+    # config.train_pn2kc = True
+    # config.sparse_pn2kc = False
+    config.save_every_epoch = True
+    # config.kc_norm_pre = 'batch_norm'
 
     hp_ranges = OrderedDict()
-    hp_ranges['dummy'] = [True]
+    hp_ranges['extra_layer'] = [False, True]
     return config, hp_ranges
 
-def plot_weights(root_path, var_name = 'w_orn', sort = True, sort_axis = 0, double_sort = 1, dir_ix = 0):
-    """Plot weights.
+path = './files_temp/relabel_extra_layer'
+t(temp1(), path, s=0, e=100)
+analysis_training.plot_sparsity(path, dynamic_thres=False)
+analysis_training.plot_distribution(path)
+sa.plot_results(path, x_key='extra_layer', y_key='val_acc')
 
-    Currently this plots OR2ORN, ORN2PN, and OR2PN
-    """
-    #TODO: fix code
-    dirs = [os.path.join(root_path, n) for n in os.listdir(root_path)]
-    save_path = dirs[dir_ix]
-    config = tools.load_config(save_path)
-    # Load network at the end of training
-    model_dir = os.path.join(save_path, 'model.pkl')
-    with open(model_dir, 'rb') as f:
-        var_dict = pickle.load(f)
-        w_plot = var_dict[var_name]
-
-    # Sort for visualization
-    if sort:
-        if sort_axis == 0:
-            ind_max = np.argmax(w_plot, axis=0)
-            ind_sort = np.argsort(ind_max)
-            w_plot = w_plot[:, ind_sort]
-            if double_sort:
-                w_plot = w_plot[ind_sort,:]
-
-    rect = [0.15, 0.15, 0.65, 0.65]
-    rect_cb = [0.82, 0.15, 0.02, 0.65]
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_axes(rect)
-    vlim = np.round(np.max(abs(w_plot)), decimals=1)
-    im = ax.imshow(w_plot, cmap='RdBu_r', vmin=-vlim, vmax=vlim,
-                   interpolation='none')
-    plt.axis('tight')
-    for loc in ['bottom', 'top', 'left', 'right']:
-        ax.spines[loc].set_visible(False)
-    ax.tick_params('both', length=0)
-    ax.set_xticks([0, w_plot.shape[1]])
-    ax.set_yticks([0, w_plot.shape[0]])
-    ax = fig.add_axes(rect_cb)
-    cb = plt.colorbar(im, cax=ax, ticks=[-vlim, vlim])
-    cb.outline.set_linewidth(0.5)
-    cb.set_label('Weight', fontsize=7, labelpad=-10)
-    plt.tick_params(axis='both', which='major', labelsize=7)
-    plt.axis('tight')
-    sa._easy_save(root_path, '_' + var_name + '_' + str(dir_ix))
-
-path = './files_temp/RNN'
-t(temp_rnn(), path, s=0, e=100)
-
-dir_ix = 0
-var_name = 'w_glo'
-# plot_weights(path, var_name = var_name, sort = False, double_sort=False, sort_axis= 0, dir_ix = dir_ix)
-# plot_weights(path, var_name = 'w_out', sort = False, double_sort=False, sort_axis= 0, dir_ix = 0)
-
-dirs = [os.path.join(path, n) for n in os.listdir(path)]
-save_path = dirs[dir_ix]
-config = tools.load_config(save_path)
-model_dir = os.path.join(save_path, 'model.pkl')
-with open(model_dir, 'rb') as f:
-    var_dict = pickle.load(f)
-    w_plot = var_dict[var_name]
-
-# ind_max = np.argmax(w_plot, axis=0)
-# ind_sort = np.argsort(ind_max)
-# w_plot = w_plot[:, ind_sort]
-# w_plot = w_plot[ind_sort,:]
-
-pn2kc = w_plot
-# pn2kc = np.sort(pn2kc, axis=0)[::-1,:]
-# plt.imshow(pn2kc)
-# plt.show()
-# mean = np.mean(pn2kc, axis=1)
-# plt.plot(mean)
-# plt.show()
-
-plt.hist(pn2kc.flatten(),bins=50, range=[.03, .5], density=False)
-plt.show()
-
-threshold = 0.05
-claw_count = np.sum(pn2kc>threshold,axis=0)
-plt.hist(claw_count, bins=50, range=[0,50], density=False)
-plt.show()
