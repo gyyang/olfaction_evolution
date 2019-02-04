37ad52f1166dd8f7de79ac5c05243ce729f6048e
gyyang <gyyang.neuro@gmail.com>
2019-02-04 12:29:57 -0500

    restructing and separating plot and compute

--- oracle/evaluatewithnoise.py
+++ oracle/evaluatewithnoise.py
@@ -24,7 +24,9 @@ mpl.rcParams['font.size'] = 7
 data_dir = os.path.join(rootpath, 'datasets', 'proto', 'standard')
 train_x, train_y, val_x, val_y = task.load_data('proto', data_dir)
 
+
 def _evaluate(name, value, model, model_dir, n_rep=1):
+    assert name is not 'weight_perturb'
     if model == 'oracle':
         path = os.path.join(rootpath, 'files', 'standard_shallow', '0')
     else:
@@ -36,10 +38,6 @@ def _evaluate(name, value, model, model_dir, n_rep=1):
     config.data_dir = rootpath + config.data_dir[1:]
     config.save_path = rootpath + config.save_path[1:]
 
-    tf.reset_default_graph()
-    # Build validation model
-    val_x_ph = tf.placeholder(val_x.dtype, val_x.shape)
-    val_y_ph = tf.placeholder(val_y.dtype, val_y.shape)
     if name == 'orn_noise_std':
         config.ORN_NOISE_STD = value
     elif name == 'orn_dropout_rate':
@@ -47,10 +45,13 @@ def _evaluate(name, value, model, model_dir, n_rep=1):
         config.orn_dropout_rate = value
     elif name == 'alpha':
         config.oracle_scale = value
-    elif name == 'weight_perturb':
-        pass
     else:
         raise ValueError('Unknown name', str(name))
+
+    tf.reset_default_graph()
+    # Build validation model
+    val_x_ph = tf.placeholder(val_x.dtype, val_x.shape)
+    val_y_ph = tf.placeholder(val_y.dtype, val_y.shape)
 
     if model == 'oracle':
         # Over-write config
@@ -74,11 +75,59 @@ def _evaluate(name, value, model, model_dir, n_rep=1):
         else:
             val_model.load()
 
+        # Validation
+        val_loss, val_acc = sess.run(
+            [val_model.loss, val_model.acc],
+            {val_x_ph: val_x, val_y_ph: val_y})
+
+    return val_loss, val_acc
+
+
+def _evaluate_weight_perturb(values, model, model_dir):
+    if model == 'oracle':
+        path = os.path.join(rootpath, 'files', 'standard_shallow', '0')
+    else:
+        path = model_dir
+
+    config = tools.load_config(path)
+
+    # TODO: clean up these paths
+    config.data_dir = rootpath + config.data_dir[1:]
+    config.save_path = rootpath + config.save_path[1:]
+
+    tf.reset_default_graph()
+    # Build validation model
+    val_x_ph = tf.placeholder(val_x.dtype, val_x.shape)
+    val_y_ph = tf.placeholder(val_y.dtype, val_y.shape)
+
+    if model == 'oracle':
+        # Over-write config
+        config.skip_orn2pn = True
+        config.skip_pn2kc = True
+        config.kc_dropout = False
+        config.set_oracle = True
+        # Helper model for oracle
+        oracle_x_ph = tf.placeholder(val_x.dtype,
+                                     [config.N_CLASS, val_x.shape[1]])
+        oracle_y_ph = tf.placeholder(val_y.dtype, [config.N_CLASS])
+        oracle = FullModel(oracle_x_ph, oracle_y_ph, config=config,
+                           training=False)
+
+    val_model = FullModel(val_x_ph, val_y_ph, config=config, training=False)
+
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    with tf.Session(config=tf_config) as sess:
+        sess.run(tf.global_variables_initializer())
+        if model == 'oracle':
+            oracle.set_oracle_weights()
+        else:
+            val_model.load()
+
         val_loss = list()
         val_acc = list()
-        for i in range(n_rep):
-            if name == 'weight_perturb':
-                val_model.perturb_weights(value)
+        for value in values:
+            val_model.perturb_weights(value)
 
             # Validation
             val_loss_tmp, val_acc_tmp = sess.run(
@@ -87,17 +136,30 @@ def _evaluate(name, value, model, model_dir, n_rep=1):
 
             val_loss.append(val_loss_tmp)
             val_acc.append(val_acc_tmp)
-    
-    return np.mean(val_loss), np.mean(val_acc)
+
+    return val_loss, val_acc
 
 
 def evaluate(name, values, model, model_dir, n_rep=1):
     losses = list()
     accs = list()
     for value in values:
-        val_loss, val_acc = _evaluate(name, value, model, model_dir, n_rep=n_rep)
+        val_loss, val_acc = _evaluate(name, value, model, model_dir,
+                                      n_rep=n_rep)
         losses.append(val_loss)
         accs.append(val_acc)
+    return losses, accs
+
+
+
+def evaluate_weight_perturb(values, model, model_dir, n_rep=1):
+    new_values = np.repeat(values, n_rep)
+
+    val_loss, val_acc = _evaluate_weight_perturb(
+        new_values, model, model_dir)
+
+    losses = np.array(val_loss).reshape(len(values), n_rep).mean(axis=1)
+    accs = np.array(val_acc).reshape(len(values), n_rep).mean(axis=1)
     return losses, accs
 
 
@@ -159,10 +221,40 @@ def evaluate_acrossmodels():
     for model_dir in model_dirs:
         config = tools.load_config(model_dir)
         model = getattr(config, model_var)
-        losses, accs = evaluate(name, values, model, model_dir, n_rep=n_rep)
+        if name == 'weight_perturb':
+            losses, accs = evaluate_weight_perturb(
+                values, model, model_dir, n_rep=n_rep)
+        else:
+            losses, accs = evaluate(name, values, model, model_dir, n_rep=n_rep)
         loss_dict[model] = losses
         acc_dict[model] = accs
         models.append(model)
+
+    results = {'loss_dict': loss_dict,
+               'acc_dict': acc_dict,
+               'models': models,
+               'model_var': model_var,
+               'values': values,
+               'name': name}
+
+    file = os.path.join(path, name + '_' + model_var + '.pkl')
+    with open(file, 'wb') as f:
+        pickle.dump(results, f)
+
+
+def plot_acrossmodels():
+    name = 'weight_perturb'
+    model_var = 'kc_inputs'
+
+    path = os.path.join(rootpath, 'files', 'vary_kc_claws_new')
+    file = os.path.join(path, name + '_' + model_var + '.pkl')
+    with open(file, 'rb') as f:
+        results = pickle.load(f)
+
+    values = results['values']
+    loss_dict = results['loss_dict']
+    acc_dict = results['acc_dict']
+    models = results['models']
 
     colors = plt.cm.cool(np.linspace(0, 1, len(values)))
     
@@ -191,6 +283,7 @@ if __name__ == '__main__':
     # evaluate_plot('alpha')
     # evaluate_plot('weight_perturb')
     # evaluate_acrossmodels('weight_perturb')
-    evaluate_acrossmodels()
+    # evaluate_acrossmodels()
+    plot_acrossmodels()
 
 
