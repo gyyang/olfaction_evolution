55fc8005a3cdd0a2a16cf15447d5c3cf8cde3348
Guangyu Robert Yang <gyyang.neuro@gmail.com>
2019-03-05 16:25:42 -0500

    vary kc claw

--- analytical/conditionnumber.py
+++ analytical/conditionnumber.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 import model
 
 
-def _get_cond(q, n_orn, n_pn, n_kc):
+def _get_cond(q, n_orn, n_pn, n_kc, n_kc_claw):
     M = np.random.rand(n_orn, n_pn)
     M_new = M * (1-q) + np.eye(n_orn) * q
     
     # J = np.random.rand(N_PN, N_KC) / np.sqrt(N_PN + N_KC)
     # J = np.random.randn(N_PN, N_KC) / np.sqrt(N_PN + N_KC)
     J = np.random.rand(n_pn, n_kc)
-    mask = model.get_sparse_mask(n_pn, n_kc, 7)
+    mask = model.get_sparse_mask(n_pn, n_kc, n_kc_claw) / n_kc_claw
     J = J * mask
 
     K = np.dot(M_new, J)
@@ -22,8 +22,8 @@ def _get_cond(q, n_orn, n_pn, n_kc):
     return cond
 
 
-def get_logcond(q=1, n_orn=50, n_pn=50, n_kc=2500, n_rep=100):
-    conds = [_get_cond(q, n_orn, n_pn, n_kc) for i in range(n_rep)]
+def get_logcond(q=1, n_orn=50, n_pn=50, n_kc=2500, n_kc_claw=7, n_rep=10):
+    conds = [_get_cond(q, n_orn, n_pn, n_kc, n_kc_claw) for i in range(n_rep)]
     return np.mean(np.log10(conds))
 
 
@@ -40,29 +40,35 @@ def plot_cond_by_q(n_kc=2500):
     # plt.savefig('figures/condvsfracdiag_nkc'+str(n_kc)+'.pdf', transparent=True)
 
 
-# =============================================================================
-# plot_cond_by_q(n_kc=30)
-# plot_cond_by_q(n_kc=2500)
-# =============================================================================
-
-
-n_kcs = np.logspace(1, 4, 10).astype(int)
-conds_q1 = np.array([get_logcond(n_kc=n_kc, q=1) for n_kc in n_kcs])
+def plot_cond_by_n_kc():
+    n_kcs = np.logspace(1, 4, 10).astype(int)
+    conds_q1 = np.array([get_logcond(n_kc=n_kc, q=1) for n_kc in n_kcs])
+    
+    plt.figure()
+    plt.plot(np.log10(n_kcs), conds_q1, 'o-')
+    plt.xticks(np.log10(n_kcs), n_kcs)
+    plt.xlabel('N_KC')
+    
+    
+    n_kcs = np.logspace(1, 4, 10).astype(int)
+    conds_q0 = np.array([get_logcond(n_kc=n_kc, q=0) for n_kc in n_kcs])
+    
+    plt.figure()
+    plt.plot(np.log10(n_kcs), conds_q0, 'o-')
+    plt.xticks(np.log10(n_kcs), n_kcs)
+    plt.xlabel('N_KC')
+    
+    plt.figure()
+    plt.plot(np.log10(n_kcs), conds_q1 - conds_q0, 'o-')
+    plt.xticks(np.log10(n_kcs), n_kcs)
+    plt.ylabel('Log decrease in condition number')
+    plt.xlabel('N_KC')
+    
+    
+n_kc_claws = np.arange(1, 21)
+conds = np.array([get_logcond(n_kc_claw=n) for n in n_kc_claws])
 
 plt.figure()
-plt.plot(np.log10(n_kcs), conds_q1, 'o-')
-plt.xticks(np.log10(n_kcs), n_kcs)
-
-
-n_kcs = np.logspace(1, 4, 10).astype(int)
-conds_q0 = np.array([get_logcond(n_kc=n_kc, q=0) for n_kc in n_kcs])
-
-plt.figure()
-plt.plot(np.log10(n_kcs), conds_q0, 'o-')
-plt.xticks(np.log10(n_kcs), n_kcs)
-
-
-plt.figure()
-plt.plot(np.log10(n_kcs), conds_q1 - conds_q0, 'o-')
-plt.xticks(np.log10(n_kcs), n_kcs)
-plt.ylabel('Log decrease in condition number')
+plt.plot(n_kc_claws, conds, 'o-')
+plt.xticks(n_kc_claws)
+plt.xlabel('N_KC_claw')
