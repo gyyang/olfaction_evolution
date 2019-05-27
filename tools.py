423f5ae7787ddbfe064c995787e05a888e16d3c5
gyyang <gyyang.neuro@gmail.com>
2019-05-27 11:51:27 -0400

    make ismodeldir more robust

--- tools.py
+++ tools.py
@@ -37,7 +37,7 @@ def _islikemodeldir(d):
     except NotADirectoryError:
         return False
     for file in files:
-        if 'model.ckpt' in file:
+        if 'model.ckpt' in file or 'log.pkl' in file:
             return True
     return False
 
