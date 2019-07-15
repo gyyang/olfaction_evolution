ca8dd5a9bf065cf7f5186d0be3f6dc8d74c19cfb
Guangyu Robert Yang <gyyang.neuro@gmail.com>
2019-07-15 18:28:48 +0800

    clean up for clarity

--- analytical/analyze_simulation_results.py
+++ analytical/analyze_simulation_results.py
@@ -26,22 +26,22 @@ mpl.rcParams['font.size'] = 7
 mpl.rcParams['pdf.fonttype'] = 42
 mpl.rcParams['ps.fonttype'] = 42
 mpl.rcParams['font.family'] = 'arial'
+# mpl.rcParams['text.usetex'] = 'true'
 
-def get_optimal_K(m, get_dim=False):
-    if get_dim:
-        fn = 'all_value_withdim_m' + str(m)
-    else:
-        fn = 'all_value_m' + str(m)
+mpl.rcParams['mathtext.fontset'] = 'stix'
+
+def get_optimal_K(filename, m, v_name):
+    fn = filename + str(m)
+
     with open('../files/analytical/'+fn+'.pkl', "rb") as f:
         all_values = pickle.load(f)
 
-    if get_dim:
-        v_name = 'dim'
+    if v_name in ['dim']:
         optimal_Ks = [v['K'][np.argmax(v[v_name])] for v in all_values]
-    else:
-        # v_name = 'E[norm_dY/norm_Y]'
-        v_name = 'theta'
+    elif v_name in ['theta']:
         optimal_Ks = [v['K'][np.argmin(v[v_name])] for v in all_values]
+    else:
+        raise ValueError('Unknown v name')
     
     means = [np.mean(np.random.choice(optimal_Ks, size=len(optimal_Ks), replace=True)) for _ in range(1000)]
     
@@ -57,12 +57,13 @@ def get_sparsity_from_training(path):
     n_ors = list()
     for i, d in enumerate(dirs):
         config = tools.load_config(d)
-        print(config.N_PN)
+        print('N: ', config.N_PN)
         sparsity = analysis_pn2kc_training.compute_sparsity(
             d, epoch=-1, dynamic_thres=False, visualize=True)
         
         n_ors.append(config.N_PN)
         sparsitys.append(sparsity[sparsity>0].mean())
+        print('Prop neurons with zero-weights: {:0.3f}'.format(np.mean(sparsity==0)))
 
     n_ors = np.array(n_ors)
     sparsitys = np.array(sparsitys)
@@ -72,23 +73,18 @@ def get_sparsity_from_training(path):
     return sparsitys[indsort], n_ors[indsort]
 
 
-def _load_result(get_dim=False):
+def _load_result(filename, v_name='theta'):
     dirs = os.listdir(os.path.join(rootpath, 'files', 'analytical'))
-    if get_dim:
-        ms = [int(d[len('all_value_withdim_m'):-len('.pkl')])
-              for d in dirs if 'all_value_withdim_m' in d]
-    else:
-        ms = [int(d[len('all_value_m'):-len('.pkl')])
-              for d in dirs if 'all_value_m' in d]
+
+    ms = [int(d[len(filename):-len('.pkl')]) for d in dirs if filename in d]
     ms = np.sort(ms)
     
-    # ms = np.array([50, 150, 1000])
     optimal_Ks = list()
     conf_ints = list()
     yerr_low = list()
     yerr_high = list()
     for m in ms:
-        optimal_K, conf_int, K_range = get_optimal_K(m, get_dim=get_dim)
+        optimal_K, conf_int, K_range = get_optimal_K(filename, m, v_name=v_name)
         print('m:' + str(m))
         print('optimal K:' + str(optimal_K))
         print('confidence interval: ' + str(conf_int))
@@ -117,17 +113,17 @@ def _load_result(get_dim=False):
     
 
 def main():
-    x, y, x_fit, y_fit, model = _load_result(get_dim=False)
+    x, y, x_fit, y_fit, model = _load_result('all_value_m', v_name='theta')
     res_perturb = {'log_N': x, 'log_K': y, 'label': 'Weight robustness'}
     res_perturb_fit = {'log_N': x_fit, 'log_K': y_fit, 'model': model,
-                       'label': 'log K = {:0.3f}log N + {:0.3f}'.format(
-                               model.coef_[0], model.intercept_)}
+                       'label': r'$K ={:0.2f} \ N^{{{:0.2f}}}$'.format(
+                               np.exp(model.intercept_), model.coef_[0])}
 
-    x, y, x_fit, y_fit, model = _load_result(get_dim=True)
+    x, y, x_fit, y_fit, model = _load_result('all_value_withdim_m', v_name='dim')
     res_dim = {'log_N': x, 'log_K': y}
     res_dim_fit = {'log_N': x_fit, 'log_K': y_fit, 'model': model,
-                   'label': 'log K = {:0.3f}log N + {:0.3f}'.format(
-                               model.coef_[0], model.intercept_)}
+                   'label': r'$K ={:0.2f} \ N^{{{:0.2f}}}$'.format(
+                               np.exp(model.intercept_), model.coef_[0])}
     
     # Get results from training
     path = os.path.join(rootpath, 'files', 'vary_n_orn2')
@@ -142,8 +138,8 @@ def main():
     model.fit(x[:, np.newaxis], y)
     y_fit = model.predict(x_fit[:, np.newaxis])
     res_train_fit = {'log_N': x_fit, 'log_K': y_fit, 'model': model,
-                     'label': 'log K = {:0.3f}log N + {:0.3f}'.format(
-                               model.coef_[0], model.intercept_)}    
+                     'label': r'$K ={:0.2f} \ N^{{{:0.2f}}}$'.format(
+                               np.exp(model.intercept_), model.coef_[0])}    
     
     file = os.path.join(rootpath, 'files', 'analytical', 'optimal_k_two_term')
     with open(file+'.pkl', 'rb') as f:
@@ -171,11 +167,11 @@ def main():
         ax.plot(res['log_N'], res['log_K'], markers[i], ms=mss[i],
                 label=labels[i], color=colors[i], zorder=zorders[i])
     ax.plot(np.log(1000), np.log(100), 'x', color=tools.darkblue)
-    ax.text(np.log(1000), np.log(120), 'Davison & Ehlers 2011', color=tools.darkblue,
+    ax.text(np.log(900), np.log(120), 'Davison & Ehlers 2011', color=tools.darkblue,
             horizontalalignment='center', verticalalignment='bottom')
     
     ax.plot(np.log(1000), np.log(40), 'x', color=tools.darkblue)
-    ax.text(np.log(1000), np.log(32), 'Miyamichi et al. 2011', color=tools.darkblue,
+    ax.text(np.log(900), np.log(32), 'Miyamichi et al. 2011', color=tools.darkblue,
             horizontalalignment='center', verticalalignment='top')
     ax.plot(np.log(50), np.log(7), 'x', color=tools.darkblue)
     ax.text(np.log(50), np.log(6), 'Caron et al. 2013', color=tools.darkblue,
@@ -196,51 +192,58 @@ def main():
     # plt.savefig(fname+'.png')    
 
     # Simplified plot 1
-    fig = plt.figure(figsize=(3.5, 2.5))
-    ax = fig.add_axes([0.2, 0.2, 0.7, 0.7])
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
+    figsize = (3.5, 2.)
+    lbwh = [0.2, 0.2, 0.7, 0.7]
+    for with_dim in [False, True]:
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_axes(lbwh)
+        ax.spines["right"].set_visible(False)
+        ax.spines["top"].set_visible(False)
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
+        
+        res_list = [res_train, res_train_fit, res_dim, res_dim_fit]
+        labels = ['Train', res_train_fit['label'],
+                  'Dimensionality', res_dim_fit['label']]
+        markers = ['+', '-', 'o', '-']
+        mss = [8, 4, 4, 4]
+        zorders = [5, 3, 1, 0]
+        colors = ['black', 'black', tools.gray, tools.gray]
+        if not with_dim:
+            res_list = res_list[:2]
+        for i, res in enumerate(res_list):
+            ax.plot(res['log_N'], res['log_K'], markers[i], ms=mss[i],
+                    label=labels[i], color=colors[i], zorder=zorders[i])
+        ax.plot(np.log(1000), np.log(100), 'x', color=tools.darkblue)
+        ax.text(np.log(900), np.log(120), 'Davison & Ehlers 2011', color=tools.darkblue,
+                horizontalalignment='center', verticalalignment='bottom')
+        ax.plot(np.log(1000), np.log(40), 'x', color=tools.darkblue)
+        ax.text(np.log(900), np.log(32), 'Miyamichi et al. 2011', color=tools.darkblue,
+                horizontalalignment='center', verticalalignment='top')
+        ax.plot(np.log(50), np.log(7), 'x', color=tools.darkblue)
+        ax.text(np.log(55), np.log(7.5), 'Caron et al. 2013', color=tools.darkblue,
+                horizontalalignment='left', verticalalignment='top')
     
-    res_list = [res_train, res_train_fit, res_dim, res_dim_fit]
-    labels = ['Train', res_train_fit['label'],
-              'Dimensionality', res_dim_fit['label']]
-    markers = ['+', '-', 'o', '-']
-    mss = [8, 4, 4, 4]
-    zorders = [5, 3, 1, 0]
-    colors = ['black', 'black', tools.gray, tools.gray]
-    for i, res in enumerate(res_list):
-        ax.plot(res['log_N'], res['log_K'], markers[i], ms=mss[i],
-                label=labels[i], color=colors[i], zorder=zorders[i])
-    ax.plot(np.log(1000), np.log(100), 'x', color=tools.darkblue)
-    ax.text(np.log(1000), np.log(120), 'Davison & Ehlers 2011', color=tools.darkblue,
-            horizontalalignment='center', verticalalignment='bottom')
-    ax.plot(np.log(1000), np.log(40), 'x', color=tools.darkblue)
-    ax.text(np.log(1000), np.log(32), 'Miyamichi et al. 2011', color=tools.darkblue,
-            horizontalalignment='center', verticalalignment='top')
-    ax.plot(np.log(50), np.log(7), 'x', color=tools.darkblue)
-    ax.text(np.log(50), np.log(6), 'Caron et al. 2013', color=tools.darkblue,
-            horizontalalignment='left', verticalalignment='top')
-
-    ax.set_xlabel('Number of ORs (N)')
-    ax.set_ylabel('Optimal K')
-    xticks = np.array([50, 100, 200, 500, 1000])
-    ax.set_xticks(np.log(xticks))
-    ax.set_xticklabels([str(t) for t in xticks])
-    yticks = np.array([3, 10, 30, 100])
-    ax.set_yticks(np.log(yticks))
-    ax.set_yticklabels([str(t) for t in yticks])
-    # ax.set_xlim([np.log(50/1.1), np.log(1000*1.1)])
-    ax.legend(bbox_to_anchor=(0., 1.05), loc=2, frameon=False)
-    fname = 'optimal_k_simulation_all_part1'
-    fname = os.path.join(rootpath, 'figures', 'analytical', fname)
-    plt.savefig(fname+'.pdf', transparent=True)
-    plt.savefig(fname+'.png')    
+        ax.set_xlabel('Number of ORs (N)')
+        ax.set_ylabel('Optimal K')
+        xticks = np.array([50, 100, 200, 500, 1000])
+        ax.set_xticks(np.log(xticks))
+        ax.set_xticklabels([str(t) for t in xticks])
+        yticks = np.array([3, 10, 30, 100])
+        ax.set_yticks(np.log(yticks))
+        ax.set_yticklabels([str(t) for t in yticks])
+        # ax.set_xlim([np.log(50/1.1), np.log(1000*1.1)])
+        ax.legend(bbox_to_anchor=(0., 1.05), loc=2, frameon=False)
+        fname = 'optimal_k_simulation_all_part1'
+        fname = os.path.join(rootpath, 'figures', 'analytical', fname)
+        if not with_dim:
+            fname = fname + 'nodim'
+        plt.savefig(fname+'.pdf', transparent=True)
+        plt.savefig(fname+'.png')    
 
     # Simplified plot 2
-    fig = plt.figure(figsize=(3.5, 2.5))
-    ax = fig.add_axes([0.2, 0.2, 0.7, 0.7])
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_axes(lbwh)
     ax.spines["right"].set_visible(False)
     ax.spines["top"].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
@@ -273,4 +276,4 @@ def main():
 
 if __name__ == '__main__':
     main()
- 
+    
