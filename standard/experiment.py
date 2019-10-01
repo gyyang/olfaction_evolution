f998f14ff033cc2b3d5c8315aba1902d56176ab3
Peter <peterwang724@gmail.com>
2019-10-01 13:38:09 -0400

    modified train.py to print out more relevant stats

--- standard/experiment.py
+++ standard/experiment.py
@@ -692,7 +692,31 @@ def vary_lr_n_kc(argTest=False, n_pn=50):
     config.save_every_epoch = False
 
     hp_ranges = OrderedDict()
-    hp_ranges['lr'] = [1e-3, 5*1e-4, 2*1e-4, 1e-4]
+    hp_ranges['lr'] = [5e-3, 2e-3, 1e-3, 5*1e-4, 2*1e-4, 1e-4]
+    hp_ranges['N_KC'] = [2500, 5000, 10000, 20000]
+    if argTest:
+        config.max_epoch = testing_epochs
+    return config, hp_ranges
+
+def vary_lr_n_kc_batchnorm(argTest=False, n_pn=50):
+    """Standard training setting"""
+    config = configs.FullConfig()
+    config.max_epoch = 100
+
+    config.N_PN = n_pn
+    config.data_dir = './datasets/proto/orn'+str(n_pn)
+
+    config.N_ORN_DUPLICATION = 1
+    config.ORN_NOISE_STD = 0.
+    config.skip_orn2pn = True
+    config.sparse_pn2kc = False
+    config.train_pn2kc = True
+    config.pn_norm_pre = 'batch_norm'
+
+    config.save_every_epoch = False
+
+    hp_ranges = OrderedDict()
+    hp_ranges['lr'] = [5e-3, 2e-3, 1e-3, 5*1e-4, 2*1e-4, 1e-4]
     hp_ranges['N_KC'] = [2500, 5000, 10000, 20000]
     if argTest:
         config.max_epoch = testing_epochs
