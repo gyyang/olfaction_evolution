e8c765f54776f6e2b35ac09ce9dd1acedca41b90
Yiliu Wang <yw2500@axon.rc.zi.columbia.edu>
2019-10-01 16:12:16 -0400

    fix

--- sbatch/vary_lr_n_kc_n_orn50011.s
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-#SBATCH --mem-per-cpu=16gb
-#SBATCH --gres=gpu:1
-#SBATCH --time=2:00:00
-
-python -c "import train; train.train_from_path('/axsys/scratch/ctn/users/yw2500/olfaction_evolution/files/vary_lr_n_kc_n_orn500/000011')"
-
-exit 0;
