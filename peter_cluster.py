852f99a11dc503443ec1cc3eb8316022317ef418
Peter <peterwang724@gmail.com>
2019-10-03 12:44:02 -0400

    new training

--- peter_cluster.py
+++ peter_cluster.py
@@ -8,7 +8,7 @@ import standard.experiment as se
 from standard.hyper_parameter_train import local_train, cluster_train
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p','--pn', nargs='+', help='N_PN', default=[50, 100])
+parser.add_argument('-p','--pn', nargs='+', help='N_PN', default=[50, 100, 200, 500])
 args = parser.parse_args()
 
 testing_epochs = 50
@@ -25,15 +25,16 @@ def temp(argTest=False, n_pn=50):
     config.skip_orn2pn = True
     config.sparse_pn2kc = False
     config.train_pn2kc = True
-    config.initializer_pn2kc = 'normal'
-    # config.initial_pn2kc = 4/n_pn # heuristic
-    # config.pn_norm_pre = 'batch_norm'
+    config.initial_pn2kc = 10/n_pn
+    config.train_kc_bias = False
 
-    config.save_every_epoch = False
+    # config.pn_norm_pre = 'batch_norm'
+    # config.save_every_epoch = True
 
     hp_ranges = OrderedDict()
-    hp_ranges['lr'] = [5e-3, 2e-3, 1e-3, 5*1e-4, 2*1e-4, 1e-4]
+    hp_ranges['lr'] = [3e-3, 1e-3, 3e-4, 1e-4, 3e-5, 1e-5]
     hp_ranges['N_KC'] = [2500, 5000, 10000, 20000]
+
     if argTest:
         config.max_epoch = testing_epochs
     return config, hp_ranges
@@ -44,11 +45,17 @@ cluster_path = '/axsys/scratch/ctn/users/yw2500/olfaction_evolution'
 n_pns = [int(x) for x in args.pn]
 print(n_pns)
 for n_pn in n_pns:
-    path = './files/cluster_initializer_normal' + str(n_pn)
+    path = './files/cluster_10_pn_untrainable_bias' + str(n_pn)
     cluster_train(temp(is_test, n_pn), path, path= cluster_path)
 
-#local_train
+## local_train
 # n_pns = [500]
 # for n_pn in n_pns:
 #     path = './files/test' + str(n_pn)
+#
+#     try:
+#         import shutil
+#         shutil.rmtree(path)
+#     except:
+#         pass
 #     local_train(temp(is_test, n_pn), path)
