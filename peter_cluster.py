538ed3e7e96e8bbfd55875ac591cd259bf8b82e9
Peter <peterwang724@gmail.com>
2019-10-06 17:30:54 -0400

    - configured cluster

--- peter_cluster.py
+++ peter_cluster.py
@@ -39,7 +39,7 @@ def temp(n_pn=50):
 def tempK(n_pn=50):
     """Standard training setting"""
     config = configs.FullConfig()
-    config.max_epoch = 50
+    config.max_epoch = 100
 
     config.N_PN = n_pn
     config.data_dir = './datasets/proto/orn'+str(n_pn)
