ad00fd7a0502aa0843e9e575296346011798591d
Peter server <peterwang724@gmail.com>
2019-10-07 00:18:09 -0400

    changed cluster

--- peter_cluster.py
+++ peter_cluster.py
@@ -14,7 +14,7 @@ args = parser.parse_args()
 def temp(n_pn=50):
     """Standard training setting"""
     config = configs.FullConfig()
-    config.max_epoch = 100
+    config.max_epoch = 200
 
     config.N_PN = n_pn
     config.data_dir = './datasets/proto/orn'+str(n_pn)
@@ -52,7 +52,7 @@ def tempK(n_pn=50):
     hp_ranges = OrderedDict()
     hp_ranges['lr'] = [3e-3, 1e-3, 3e-4, 1e-4, 3e-5, 1e-5]
     hp_ranges['N_KC'] = [2500, 5000, 10000]
-    hp_ranges['initial_K'] = [1, n_pn/2]
+    hp_ranges['initial_K'] = [n_pn, n_pn/2, n_pn/4]
     return config, hp_ranges
 
 
@@ -61,7 +61,7 @@ cluster_path = '/axsys/scratch/ctn/users/yw2500/olfaction_evolution'
 n_pns = [int(x) for x in args.pn]
 print(n_pns)
 for n_pn in n_pns:
-    path = './files/cluster_parameterize_K' + str(n_pn)
+    path = './files/cluster_parameterize_K_vary_initial_K' + str(n_pn)
     cluster_train(tempK(n_pn), path, path= cluster_path)
 
 ## local_train
