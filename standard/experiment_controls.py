ea4d609882cde9f2921a4573262a6d150fba5a1f
gryang <gyyang.neuro@gmail.com>
2020-06-09 16:36:21 -0400

    new experiment corr w/o sign constraint

--- standard/experiment_controls.py
+++ standard/experiment_controls.py
@@ -675,6 +675,30 @@ def vary_orn_corr():
     return configs
 
 
+def vary_orn_corr_nosign():
+    config = FullConfig()
+    config.data_dir = './datasets/proto/standard'
+    config.max_epoch = 200
+    config.pn_norm_pre = 'batch_norm'
+
+    config.batch_size = 8192
+    config.N_ORN_DUPLICATION = 1
+    config.ORN_NOISE_STD = 0
+    config.lr = 2 * 1e-3
+    config.initial_pn2kc = 10. / config.N_PN
+    config.initializer_pn2kc = 'uniform'
+    config.sign_constraint_orn2pn = False
+
+    orn_corrs = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    datasets = ['./datasets/proto/orn_corr_{:0.2f}'.format(c) for c in orn_corrs]
+
+    config_ranges = OrderedDict()
+    config_ranges['orn_corr'] = orn_corrs
+    config_ranges['data_dir'] = datasets
+    configs = vary_config(config, config_ranges, mode='sequential')
+    return configs
+
+
 def vary_orn_corr_analysis(path):
     xkey = 'orn_corr'
     ykeys = ['val_acc', 'K_inferred', 'glo_score']
